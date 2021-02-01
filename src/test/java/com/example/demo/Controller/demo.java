680d5fb74f00795eac82a5cc8440bbb98ae926ba
Kevin Friedman <kevin501st@hotmail.com>
2021-01-31 23:17:16 -0500

    testing testing

---
+++ src/test/java/com/example/demo/Controller/demo.java
@@ -0,0 +1,14 @@
+package com.example.demo.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+// mark this class as a controller to handle /demo requests
+@RestController
+@RequestMapping(value = "/demo")
+public class demo {
+    // create GET endpoint to serve demo data at /demo/data
+    @GetMapping(value = "/data")
+    public String getDemoData() {
+        return "Team Ford. Capstone Spring 2021. Team View Project";
+    }
+}
